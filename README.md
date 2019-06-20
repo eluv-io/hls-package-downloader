06d5bf109cbf5919c59eb79f4f464fedd822e2cb
Peter Tseng <peter@eluv.io>
2019-06-19 22:50:55 -0700

    Add authorization header support; Multithreading

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # hls-package-downloader
 Download an HLS playlist and segment files for all renditions and variant streams
+
+```
+usage: hpd.py [-h] [-a AUTH] [-t THREADS] url
+
+positional arguments:
+  url                   the HLS playlist/manifest URL, e.g.
+                        https://devstreaming-cdn.apple.com/videos/streaming/ex
+                        amples/img_bipbop_adv_example_ts/master.m3u8
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -a AUTH, --auth AUTH  set a bearer token authorization header with each HTTP
+                        request
+  -t THREADS, --threads THREADS
+                        the maximum number of concurrent downloads
+```
--- hpd.py
+++ hpd.py
@@ -6,37 +6,50 @@
 # Http Live Streaming -- fetcher/downloader
 # A simple script to download segments/m3u8 files from given url, including
 #   variants and alternative renditions.
+#
+# TODO
+#   * Support packages with all segments in a single file with byte range requests
+#   * Add output directory option
+#   * Add logging verbosity option
 
-import sys
+import argparse
+import logging
 import os
+import threading
 import urllib.request
 
-TEST_URL = "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/master.m3u8"
+auth_header = ""
+lock = threading.Lock()
+threads = list()
+max_threads = 10
 
 
 def isValidUrl(url: str) -> bool:
     if url == "":
-        print("Invalid URL: empty url")
+        logging.error("Invalid URL: empty url")
         return False
     elif not (url.startswith("http") or url.startswith("https")):
-        print("Invalid URL: require 'http/https' url")
+        logging.error("Invalid URL: require 'http/https' url")
         return False
     elif os.path.splitext(url)[1].lower() != ".m3u8":
-        print("Invalid URL: not hls source")
+        logging.error("Invalid URL: not hls source")
         return False
     else:
         return True
 
 
 def readDataFromUrl(url: str) -> bytes:
-    with urllib.request.urlopen(url) as response:
+    headers = {}
+    if len(auth_header) > 0:
+        headers["authorization"] = auth_header
+    request = urllib.request.Request(url=url, headers=headers)
+    with urllib.request.urlopen(request) as response:
         data = response.read()
     return data
 
 
-def writeFile(path: str, filename: str, data: bytes) -> None:
-    fullPath = os.path.join(path, filename)
-    with open(fullPath, "wb") as file:
+def writeFile(path: str, data: bytes) -> None:
+    with open(path, "wb") as file:
         file.write(data)
     return None
 
@@ -56,7 +69,7 @@ def parsePlaylist(baseDir: str, relativePath: str, baseUrl: str, data: bytes) ->
                 if len(attrSplit) != 2:
                     break
                 # TODO Only handles relative paths right now
-                uri = attrSplit[1].strip(b'"').decode()
+                uri = attrSplit[1].strip().strip(b'"').decode()
                 fullUrl = os.path.join(baseUrl, relativePath, uri)
                 fetch(fullUrl, baseDir, baseUrl)
         elif extension.lower() == b".m3u8":
@@ -64,42 +77,92 @@ def parsePlaylist(baseDir: str, relativePath: str, baseUrl: str, data: bytes) ->
             fetch(playlistUrl, baseDir, baseUrl)
         elif len(extension) > 0:
             segUrl = os.path.join(baseUrl, relativePath, line.decode())
-            fetch(segUrl, baseDir, baseUrl)
+            threadedFetch(segUrl, baseDir, baseUrl)
+
+
+def threadedFetch(url: str, baseDir: str, baseUrl: str) -> None:
+    thread = None
+    while True:
+        with lock:
+            if len(threads) >= max_threads:
+                thread = threads.pop(0)
+            else:
+                thread = threading.Thread(target=fetch, args=(url, baseDir, baseUrl))
+                threads.append(thread)
+                break
+        thread.join()
+    thread.start()
 
 
 def fetch(url: str, baseDir: str, baseUrl: str) -> None:
     # print("\nfetch", "\nurl:", url, "\nbaseDir:", baseDir, "\nbaseUrl:", baseUrl)
     filename = os.path.basename(url)
-    relPath = os.path.dirname(url)[len(baseUrl):].strip('/')
-    writeDir = os.path.join(baseDir, relPath)
-    if not os.path.exists(writeDir):
-        os.makedirs(writeDir)
-    print("Downloading", url, "to", writeDir)
-    data = readDataFromUrl(url)
-    writeFile(writeDir, filename, data)
-
     extension = os.path.splitext(filename)[1]
+    relPath = os.path.dirname(url)[len(baseUrl) :].strip("/")
+    outDir = os.path.join(baseDir, relPath)
+    if not os.path.exists(outDir):
+        os.makedirs(outDir)
+    outPath = os.path.join(outDir, filename)
+    if os.path.exists(outPath) and extension.lower() != ".m3u8":
+        logging.debug("Skipping %s, %s already exists", url, outPath)
+        return
+    logging.debug("Downloading %s to %s", url, outDir)
+    data = readDataFromUrl(url)
+    writeFile(outPath, data)
     if extension.lower() == ".m3u8":
         parsePlaylist(baseDir, relPath, baseUrl, data)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print(
-            "Invalid arguments: require 1 parameter, but you gave ", len(sys.argv) - 1
-        )
-        exit(-1)
-    url = sys.argv[1]
-    # url = TEST_URL
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "url",
+        help="the HLS playlist/manifest URL, e.g. https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_ts/master.m3u8",
+        type=str,
+    )
+    parser.add_argument(
+        "-a",
+        "--auth",
+        help="set a bearer token authorization header with each HTTP request",
+        type=str,
+    )
+    parser.add_argument(
+        "-t", "--threads", help="the maximum number of concurrent downloads", type=int
+    )
+    args = parser.parse_args()
+    url = args.url
+    if args.auth:
+        auth_header = "Bearer " + args.auth
+    if args.threads:
+        max_threads = args.threads
+
+    logging.basicConfig(
+        format="%(asctime)s %(levelname)s %(message)s",
+        level=logging.DEBUG,
+        datefmt="%H:%M:%S",
+    )
+
     if isValidUrl(url):
-        # http://.../hls-aes/playlist.m3u8
-        # http://.../hls-aes
+        # url:       http://.../hls-aes/playlist.m3u8
+        # baseUrl:   http://.../hls-aes
         baseUrl = os.path.dirname(url)
-        # /a/b/c
+
+        # curPath:   /a/b/c
         curPath = os.path.abspath(os.curdir)
-        # /a/b/c/hls-aes
-        writeDir = os.path.join(curPath, os.path.basename(baseUrl))
-        fetch(url, writeDir, baseUrl)
-        print("Done")
+
+        # outDir:    /a/b/c/hls-aes
+        outDir = os.path.join(curPath, os.path.basename(baseUrl))
+
+        fetch(url, outDir, baseUrl)
+
+        while True:
+            thread = None
+            with lock:
+                if len(threads) == 0:
+                    break
+                else:
+                    thread = threads.pop(0)
+            thread.join()
+        logging.info("Done")
     else:
         exit(-1)
